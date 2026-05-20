8d3649bb6bd9c7d7df8f1194c7dc074d787c274f
sondre <sondret321@gmail.com>
2026-05-20 02:01:07 +0200

    exercise 1.13

--- Exercises/exercise1.13.c
+++ Exercises/exercise1.13.c
@@ -1,2 +1,44 @@
-// Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is
+//  Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is
 // easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging.
+
+#include "include/exercise1.13.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+    int *lengths = input(argc, argv);
+
+    for (int i = 1; i < MAX_LEN; i++) {
+        if (lengths[i] > 0){
+            printf("%d: ", i);
+
+            for (int j = 0; j < lengths[i]; j++) {
+                putchar('*');
+            }
+
+            putchar('\n');
+        }
+    }
+    free(lengths);
+    return 0;
+}
+
+
+int *input(int argc, char *argv[]){
+    int *lengths = calloc(100, sizeof(int));
+    if (!lengths) {
+        perror("calloc");
+        exit(EXIT_FAILURE);
+    }
+    if (argc < 2) {
+        fprintf(stderr, "Usage:%s < Words >\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 1; i < argc; i++) {
+        int len = strlen(argv[i]);
+        if (len < 100) {
+            lengths[len]++;
+        }
+    }
+    return lengths;
+}
---
+++ Exercises/include/exercise1.13.h
@@ -0,0 +1,8 @@
+#ifndef _Exercise1_13_
+#define _Exercise1_13_
+
+#define MAX_BUFFER 256
+#define MAX_LEN 100
+
+int *input(int argc, char *argv[]);
+#endif // _Exercise1_13_
